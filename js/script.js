058765753dfcb13dc81eefbbb8e51bd230746d92
NicoloYan <nico.yan@outlook.it>
2022-06-14 15:30:59 +0200

    Data collection phase complete

--- js/script.js
+++ js/script.js
@@ -9,3 +9,40 @@
 // se il numero è presente nella lista dei numeri generati, abbiamo calpestato una bomba, il gioco finisce con un messaggio di errore
 // Altrimenti il gioco va avanti a meno di aver raggiunto il numero massimo di tentativi possibili. In questo caso il gioco finisce con un messaggio di vittoria.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha scelto un numero che non era una bomba.
+
+// Chiedo all'utente il livello per la difficoltà del gioco
+let gameLevel = parseInt(prompt('Scegli la difficoltà del gioco (1 - 2 - 3)'))
+
+// Definisco in numberOfBombs il numero di bombe presenti nel campo minato
+const numberOfBombs = 16;
+
+// Definisco minRange e maxRange
+const minRange = 1
+let maxRange
+
+if (gameLevel === 1) {
+    maxRange = 100
+} else if (gameLevel === 2) {
+    maxRange = 81
+} else if (gameLevel === 3) {
+    maxRange = 49
+}
+console.log(maxRange)
+
+// Definisco il numero massimo di tentativi possibili e lo salvo in una variabile
+let maxAttempts = maxRange - numberOfBombs
+
+
+
+
+
+
+// ----------
+// FUNCTIONS
+// ----------
+
+function bombGenerator(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
